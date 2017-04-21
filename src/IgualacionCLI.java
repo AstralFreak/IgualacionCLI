b5ce72621005f777fb0af9ffdfc8ff800da3e6c2
AstralFreak <pablonogueira@outlook.com>
2017-04-21 13:57:26 -0500

    Simplificación de las variables, Implementacion de Scanner para la entrada de los incisos

--- src/IgualacionCLI.java
+++ src/IgualacionCLI.java
@@ -4,6 +4,7 @@
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Scanner;
 
 public class IgualacionCLI {
 
@@ -14,22 +15,28 @@ public class IgualacionCLI {
             String NommbreSujeto;
             String ApellidosSujeto;
             int NumeroCaso;
-            String NombreAplicador;
-            String ApellidoAplicador;
+            int NumeroAplicador;
             String Inciso;
-        /*Se asignan valores temporales a las variables con el proposito de demostracion */
 
-            NommbreSujeto = "Carlos";
-            ApellidosSujeto ="Rodriguez";
-            NumeroCaso = 10101010;
-            NombreAplicador = "Pancho";
-            ApellidoAplicador = "Barraza";
+
             Inciso = "Cual de estos es mas grande?";
 
 
+            Scanner in = new Scanner(System.in);
             System.out.println("!Bienvenido! ¿Estas listo para comenzar?");
             System.out.println("Por favor intruduzca los datos que se le solicitan a continuacion para realizar el registro apropiado.");
             System.out.println("Despues de introducir los datos, se presentaran las instrucciones al sujeto.");
+
+            System.out.println("Nombre del sujeto");
+            NommbreSujeto = in.nextLine();
+            System.out.println("Apellidos del sujeto");
+            ApellidosSujeto = in.nextLine();
+            System.out.println("Número de caso");
+            NumeroCaso = in.nextInt();
+            System.out.println("Número del aplicador");
+            NumeroAplicador = in.nextInt();
+
+
             System.out.println("A continuacion te mostraremos 2 numeros. Dino por favor cual es el mayor");
 
         /*Este bloque crea el timestamp para el inicio del ensayo*/
@@ -39,16 +46,23 @@ public class IgualacionCLI {
 
 
         /*inciso 1*/
+        int Inciso1;
             System.out.println(Inciso);
             System.out.println("42  o   32");
+            Inciso1 = in.nextInt();
+
 
         /*inciso 2*/
+        int Inciso2;
             System.out.println(Inciso);
             System.out.println("22  o   32");
+            Inciso2 = in.nextInt();
 
         /*inciso 3*/
+        int Inciso3;
             System.out.println(Inciso);
             System.out.println("1000  o   0001");
+            Inciso3 = in.nextInt();
 
 /* codigo para el reporte final*/
 
@@ -63,8 +77,11 @@ public class IgualacionCLI {
             System.out.println("Expediente: " + NumeroCaso);
 
         /*Esta Linea muestra el Nombre y apellido del aplicador*/
-            System.out.print("Aplicador: " + NombreAplicador);
-            System.out.println(" " + ApellidoAplicador);
+            System.out.println("Aplicador: " + NumeroAplicador);
+
+         /*Esta linea muestra las respuestas*/
+
+            System.out.println("Respuesta 1: " + Inciso1 + "  Respuesta 2: " + Inciso2 + "  Respuesta 3: "  + Inciso3);
 
 
     }
